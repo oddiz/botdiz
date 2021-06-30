9e1a76e07e3a63753f137ec9e8a48aef3f8ce576
oddiz <kaansarkaya@gmail.com>
2021-06-30 08:49:52 +0300

    dsadadasdasd

--- server_src/routes/Websocket/ListenerManager.js
+++ server_src/routes/Websocket/ListenerManager.js
@@ -10,6 +10,7 @@ module.exports = class ListenerManager {
         
         
 
+        this.processMessage = this.processMessage.bind(this)
         this.client.on("message", this.processMessage) 
 
         
