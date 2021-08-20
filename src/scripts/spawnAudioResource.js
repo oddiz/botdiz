e04f479b401afedccc9f7884675171eead600e72
oddiz <kaansarkaya@gmail.com>
2021-08-20 03:28:09 +0300

    ytdl console log cleanup

--- src/scripts/spawnAudioResource.js
+++ src/scripts/spawnAudioResource.js
@@ -4,23 +4,22 @@ module.exports = function spawnAudioResource(nextInQueue) {
     return new Promise(async (resolve, reject) => {
         
         
-        const videoInfo = await ytdl.getInfo(nextInQueue.videoUrl)
-        const audioFormats = ytdl.filterFormats(videoInfo.formats, 'audio');
-        const chosenFormat = ytdl.chooseFormat(audioFormats, {quality: "highestaudio"})
-        console.log(chosenFormat)
-        const stream = ytdl(nextInQueue.videoUrl, { filter: 'audioonly', dlChunkSize:0, liveBuffer:10000, quality:'highestaudio'})
+        // const videoInfo = await ytdl.getInfo(nextInQueue.videoUrl)
+        // const audioFormats = ytdl.filterFormats(videoInfo.formats, 'audio');
+        // const chosenFormat = ytdl.chooseFormat(audioFormats, {quality: "highestaudio"})
+        const stream = ytdl(nextInQueue.videoUrl, { filter: 'audio', dlChunkSize:0, liveBuffer:10000, quality:'highestaudio'})
         
         
         const onError = (error) => {
             console.log("Error while trying to spawn Audio Resource" , error)
 
-            stream.resume();
+            stream.destroy();
             reject(error);
         };
         
-            demuxProbe(stream)
-            .then((probe) => resolve(createAudioResource(probe.stream, { inputType: probe.type })))
-            .catch(onError);
+        demuxProbe(stream)
+        .then((probe) => resolve(createAudioResource(probe.stream, { inputType: probe.type })))
+        .catch(onError);
             
     });
 }
