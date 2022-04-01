28547cada732c35acdffe353f5a5a88516f839e7
Kaan Şarkaya <kaansarkaya@gmail.com>
2022-04-01 03:37:57 +0300

    spotify auth debugging

--- src/modules/SpotifyApiHandler.ts
+++ src/modules/SpotifyApiHandler.ts
@@ -21,12 +21,14 @@ class SpotifyApiManager {
     }
 
     private async refreshToken() {
+        const date = new Date();
         this.spotifyApi
             .clientCredentialsGrant()
             .then((data) => {
                 this.access_token = data.body['access_token'];
-                this.access_token_expiration = Date.now() + data.body['expires_in'] * 1000;
+                this.access_token_expiration = date.getTime() + data.body['expires_in'] * 1000;
 
+                console.log('Got new spotify access token.');
                 this.spotifyApi.setAccessToken(this.access_token);
             })
             .catch((err) => {
@@ -35,11 +37,27 @@ class SpotifyApiManager {
     }
 
     private tokenRefreshNeeded() {
-        if (this.access_token_expiration < Date.now()) return true;
+        const date = new Date();
 
-        if (!this.access_token) return true;
+        console.log('Checking if token refresh needed');
 
-        if (this.access_token_expiration === 0) return true;
+        if (this.access_token_expiration < date.getTime()) {
+            console.log('Yes, token is expired');
+            return true;
+        }
+
+        if (!this.access_token) {
+            console.log('Yes, token is empty');
+            return true;
+        }
+
+        if (this.access_token_expiration === 0) {
+            console.log('Yes, token expiration is 0');
+            return true;
+        }
+
+        console.log('No, token is not expired');
+        console.log('expiry  :' + this.access_token_expiration + '\ncurrent :' + date.getTime());
 
         return false;
     }
