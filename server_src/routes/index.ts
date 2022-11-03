84221634d18daca511a474f3eaac82144cb23547
Kaan <kaansarkaya@gmail.com>
2022-11-04 00:32:03 +0300

    fix: removed redundant logs

--- server_src/routes/index.ts
+++ server_src/routes/index.ts
@@ -1,45 +1,38 @@
-import { Db } from 'mongodb';
+import { Db } from "mongodb";
 
-import login from './login';
-import validate from './validate';
-import logout from './logout';
-import playlists from './playlists';
-import addsuperuser from './addsuperuser';
-import discordlogin from './discordlogin';
-import discordguild from './discordguild';
-import botdizguild from './botdizguild';
-import botdizstats from './botdizstats';
-import metrics from './metrics';
-import dotenv from 'dotenv';
+import login from "./login";
+import validate from "./validate";
+import logout from "./logout";
+import playlists from "./playlists";
+import addsuperuser from "./addsuperuser";
+import discordlogin from "./discordlogin";
+import discordguild from "./discordguild";
+import botdizguild from "./botdizguild";
+import botdizstats from "./botdizstats";
+import metrics from "./metrics";
+import dotenv from "dotenv";
 
-import { APIRateLimiter } from '../RateLimiter';
-import { Express, RequestHandler } from 'express';
-import { BotdizSession } from 'server_src/types';
-import { logger } from '../../src/logger';
-import { appengine } from 'googleapis/build/src/apis/appengine';
+import { APIRateLimiter } from "../RateLimiter";
+import { Express, RequestHandler } from "express";
+import { BotdizSession } from "server_src/types";
 dotenv.config();
 
 const rateLimiter: RequestHandler = (req, res, next) => {
     const session = req.session as BotdizSession;
 
-    const whitelistedRoutes = ['/login', '/discordlogin', '/validate'];
+    const whitelistedRoutes = ["/login", "/discordlogin", "/validate"];
 
     if (whitelistedRoutes.includes(req.path)) {
         next();
     } else if (session.userId) {
-        if (
-            APIRateLimiter.isUserAllowed(session.userId) ||
-            req.path === '/discordlogin' ||
-            req.path === '/login'
-        ) {
+        if (APIRateLimiter.isUserAllowed(session.userId)) {
             next();
         } else {
             //logger.log('warn', 'Rate limited user: ' + session.userId + '\nPath: ' + req.path);
-            res.status(401).send({ status: 'rate_limited' });
+            res.status(401).send({ status: "rate_limited" });
         }
     } else {
-        logger.log('warn', 'No user id in session. Path: ' + req.path);
-        res.status(401).send({ status: 'rate_limited' });
+        next();
     }
 };
 
